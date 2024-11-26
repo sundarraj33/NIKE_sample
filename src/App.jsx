c785e8c7ec1fe061f1a224d59e426e6bfe6e0e76
sundarraj33 <sundarraj.m033@gmail.com>
2024-11-27 00:39:09 +0530

    Sample NIKE Project

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import "../src/App.css";
 import Navbar2 from "./Pages/Navbar2";
 import Navbar3 from "./Pages/Navbar3";
 import Navbar4 from "./Pages/Navbar4";
+import Main from "./Pages/Main";
 const App = ()=>{
   return(
     <>    
@@ -10,6 +11,7 @@ const App = ()=>{
     <Navbar2 />
     <Navbar3 />
     <Navbar4 /> 
+    <Main />
       
     
     </>
---
+++ src/Pages/Main.jsx
@@ -0,0 +1,19 @@
+const Main =()=>{
+    return (
+        <>        
+            <div className="container m-auto">
+                <div className="wrapper h-96 border border-gray-500 flex">
+                    <div className="right w-2/4">
+                        <img src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/bb5326cb-8bb3-4cf3-b438-05d35d2899db/W+J+BRKLN+FLC+CREW+GIFT+GFX.png" className="h-96"></img>
+                    </div>
+                    <div className="right w-2/4" style={{"background-color":"red","width":"500px"}}>
+                    <img src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/c78b3c73-c930-441c-ab05-c44f8eb12e16/JORDAN+TATUM+3+PF.png" className="h-96" ></img>
+                    </div>
+                </div>
+            </div>
+        
+        </>
+    )
+}
+
+export default Main;
