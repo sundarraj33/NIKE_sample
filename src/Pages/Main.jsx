77c68d46cffe561b27a5724754fb676a3cdff091
sundarraj33 <sundarraj.m033@gmail.com>
2024-11-27 00:43:06 +0530

    Sample NIKE Project

--- src/Pages/Main.jsx
+++ src/Pages/Main.jsx
@@ -7,7 +7,18 @@ const Main =()=>{
                         <img src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/bb5326cb-8bb3-4cf3-b438-05d35d2899db/W+J+BRKLN+FLC+CREW+GIFT+GFX.png" className="h-96"></img>
                     </div>
                     <div className="right w-2/4" style={{"background-color":"red","width":"500px"}}>
-                    <img src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/c78b3c73-c930-441c-ab05-c44f8eb12e16/JORDAN+TATUM+3+PF.png" className="h-96" ></img>
+                    <img src="https://static.nike.com/a/images/t_PDP_1728_v1/f_auto,q_auto:eco,u_126ab356-44d8-4a06-89b4-fcdcc8df0245,c_scale,fl_relative,w_1.0,h_1.0,fl_layer_apply/c78b3c73-c930-441c-ab05-c44f8eb12e16/JORDAN+TATUM+3+PF.png" className="h-96"   ></img>
+                    </div>
+                </div>
+
+                <div className="para1 m-4">
+                    <div className="flex-row ">
+                        <div className="justify-content">
+                        <p>Tatum 3</p>
+                        <p>ZERO PRESSURE</p>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quidem impedit dolorum quam modi explicabo illum deleniti voluptate magnam amet libero, obcaecati earum recusandae nobis, voluptatum in iste. Possimus, quidem in.</p>
+                        <button className="">Shop Now</button>
+                        </div>
                     </div>
                 </div>
             </div>
