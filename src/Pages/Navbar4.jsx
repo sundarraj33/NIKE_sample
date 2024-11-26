86bd8818d3fd53d97b743fe18924d6aed97b5e90
sundarraj33 <sundarraj.m033@gmail.com>
2024-11-27 00:20:59 +0530

    Sample NIKE Project

--- src/Pages/Navbar4.jsx
+++ src/Pages/Navbar4.jsx
@@ -1,15 +1,22 @@
 const Navbar4 = ()=>{
     return(
         <>
-        <div className="conatiner">
-        <img src='https://upload.wikimedia.org/wikipedia/en/thumb/3/37/Jumpman_logo.svg/375px-Jumpman_logo.svg.png?20090410062117' className='h-24'></img>
-        <ul>
-            <li>New Releases</li>
-            <li>Jordan Sport</li>
-            <li>Man</li>
-            <li>Women</li>
-            <li>Kids</li>
-        </ul>
+        <div className="conatiner m-auto flex justify-center m-3">
+            <div className="flex-row">
+                <div className="flex justify-center m-3">
+                <img src='https://upload.wikimedia.org/wikipedia/en/thumb/3/37/Jumpman_logo.svg/375px-Jumpman_logo.svg.png?20090410062117' className='h-32 '></img>
+                </div>
+                <div className="">
+                <ul className="flex justify-between">
+                    <li className="p-2 text-sm">New Releases</li>
+                    <li className="p-2 text-sm">Jordan Sport</li>
+                    <li className="p-2 text-sm">Man</li>
+                    <li className="p-2 text-sm">Women</li>
+                    <li className="p-2 text-sm">Kids</li>
+                </ul>
+                </div>
+                
+            </div>        
         </div>
             
         </>
